45306bef00d8c83b4feb0691ee4f97dee252778b
Kosmo <The_Kosmo@outlook.com>
2023-06-23 19:25:27 +0500

    Добавьте файлы проекта.

---
+++ src/module32.cpp
@@ -0,0 +1,19 @@
+﻿// module32.cpp: определяет точку входа для приложения.
+//
+
+#include "../include/module32.h"
+#include "../include/nlohmann/json.hpp"
+
+int main()
+{
+	std::ifstream file("films.json");
+	if (!file.is_open())
+	{
+		std::cerr << "Dont open" << std::endl;
+	}
+	nlohmann::json dic;
+	file >> dic;
+	std::cout << "Here";
+	
+	return 0;
+}
